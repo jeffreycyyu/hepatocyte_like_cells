5d12e78689b07f50651ec52a9519666a3276622b
Jeffrey C.Y. Yu <83930692+jeffreycyyu@users.noreply.github.com>
2023-08-17 10:04:01 -0400

    Create multiomic_differential_expression_compare_to_specific.R

---
+++ multiomic_analysis/multiomic_differential_expression_compare_to_specific.R
@@ -0,0 +1,97 @@
+rm(list = ls())
+
+
+library(monocle3)
+library(Seurat)
+library(Signac)
+library(ggplot2)
+library(dplyr)
+library(EnsDb.Hsapiens.v86)
+library(biovizBase)
+library(ggforce)
+library(foreach)
+library(doParallel)
+
+#setup parallel backend to use many processors
+cores=detectCores()
+cl <- makeCluster(cores[1]-1) #not to overload your computer
+registerDoParallel(cl)
+
+
+# =========================
+# LOAD DATA AND INFO
+# =========================
+load("/home/jyu/scratch/hepatocyte_like_cells/multiomic_analysis/seurat_formatted_data.RData")
+
+DefaultAssay(all_rna) <- "MAGIC_rna"
+
+
+# =========================
+# DIFFERENTIAL EXPRESSION
+# =========================
+
+#dataframe of all unique cell type comparisons
+differential_expression_comparisons = as.data.frame(t(combn(order_cell_types, 2)))
+
+
+
+#
+# #differential expression analysis (total)
+# #rna: find all group markers compared to all other cells
+# all_markers_rna = Seurat::FindAllMarkers(all_rna, test.use = "bimod")
+# write.table(all_markers_rna, "/home/jyu/scratch/hepatocyte_like_cells/multiomic_analysis/differentially_expressed/rna/all_markers_rna.txt", sep = "\t", row.names = T, col.names = T, quote = F)
+# print("rna: finished total differential expression analysis")
+# #=====
+# #atac: find all group markers compared to all other cells
+# all_markers_atac = Seurat::FindAllMarkers(all_atac, test.use = "bimod")
+# write.table(all_markers_atac, "/home/jyu/scratch/hepatocyte_like_cells/multiomic_analysis/differentially_expressed/atac/all_markers_atac.txt", sep = "\t", row.names = T, col.names = T, quote = F)
+# print("atac: finished total differential expression analysis")
+#
+# print("FINISHED: rna and atac differential expression analysis (total)")
+#
+#
+
+
+#==================================
+
+
+
+#differential expression analysis (pairwise)
+#rna
+print("rna: starting differential expression analysis")
+foreach(k = 1:nrow(differential_expression_comparisons)) %dopar% {
+  i = differential_expression_comparisons[k, 1]
+  j = differential_expression_comparisons[k, 2]
+
+  print(paste0("starting rna: ", i, " vs. ", j))
+
+  differentially_expressed_ij <- Seurat::FindMarkers(all_rna, ident.1 = i, ident.2 = j)
+  differentially_expressed_ij$gene_short_name = gene_translations[match(rownames(differentially_expressed_ij), gene_translations$id), "gene_short_name"]
+  write.table(differentially_expressed_ij, paste0("/home/jyu/scratch/hepatocyte_like_cells/multiomic_analysis/differentially_expressed/rna/rna_differentially_expressed_", i, "_", j, ".txt"), sep = "\t", row.names = T, col.names = T, quote = F)
+
+  print(paste0("finished rna: ", i, " vs. ", j))
+}
+print("rna: finished pairwise differential expression analysis")
+#======
+#atac
+print("atac: starting differential expression analysis")
+foreach(k = 1:nrow(differential_expression_comparisons)) %dopar% {
+
+  i = differential_expression_comparisons[k, 1]
+  j = differential_expression_comparisons[k, 2]
+
+  print(paste0("starting atac: ", i, " vs. ", j))
+
+  differentially_expressed_ij <- Seurat::FindMarkers(all_atac, ident.1 = i, ident.2 = j)
+  write.table(differentially_expressed_ij, paste0("/home/jyu/scratch/hepatocyte_like_cells/multiomic_analysis/differentially_expressed/atac/atac_differentially_expressed_", i, "_", j, ".txt"), sep = "\t", row.names = T, col.names = T, quote = F)
+
+  print(paste0("finished atac: ", i, " vs. ", j))
+}
+print("atac: finished pairwise differential expression analysis")
+
+print("FINISHED: rna and atac differential expression analysis (pairwise)")
+
+
+#=============
+
+print("MASTER: done everything")
