d43f96ceba227c4fa2f6c7a04b4a6ca0b0f93a52
Jeffrey C.Y. Yu <83930692+jeffreycyyu@users.noreply.github.com>
2023-08-17 10:04:38 -0400

    Create multiomic_differential_expression_compare_to_specific.sh

---
+++ multiomic_analysis/multiomic_differential_expression_compare_to_specific.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --account=def-rsladek   # replace this with your own account
+#SBATCH --mem-per-cpu=400G      # memory; default unit is megabytes
+#SBATCH --time=0-18:00         # time (DD-HH:MM)
+#SBATCH --output=multiomic_differential_expression_compare_to_specific.out
+
+
+Rscript multiomic_differential_expression_compare_to_specific.R
