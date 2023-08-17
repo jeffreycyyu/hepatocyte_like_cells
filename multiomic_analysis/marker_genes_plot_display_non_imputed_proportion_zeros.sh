8d3a56a4dfc7636dda1996d05f8e25ba40f12488
Jeffrey C.Y. Yu <83930692+jeffreycyyu@users.noreply.github.com>
2023-08-17 09:47:45 -0400

    Create marker_genes_plot_display_non_imputed_proportion_zeros.sh

---
+++ multiomic_analysis/marker_genes_plot_display_non_imputed_proportion_zeros.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --account=def-rsladek   # replace this with your own account
+#SBATCH --mem-per-cpu=140G      # memory; default unit is megabytes
+#SBATCH --time=0-3:00         # time (DD-HH:MM)
+#SBATCH --output=marker_genes_plot_display_non_imputed_proportion_zeros.out
+
+
+Rscript marker_genes_plot_display_non_imputed_proportion_zeros.R
